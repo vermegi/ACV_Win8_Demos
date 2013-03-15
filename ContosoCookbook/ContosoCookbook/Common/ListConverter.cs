46d3384da99a52a2040c817ab7bc41bef09aca8c
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2013-03-15 15:18:44 +0100

    After demo2
    Better views for recipe data.

---
+++ ContosoCookbook/ContosoCookbook/Common/ListConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace ContosoCookbook.Common
+{
+    public class ListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var items = (ObservableCollection<string>) value;
+            var bld = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                bld.Append(item);
+                bld.Append("\r\n");
+            }
+
+            return bld.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
